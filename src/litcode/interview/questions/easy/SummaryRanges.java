5420993e1c38f2ffcd429dedee6d3643be55c7de
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-09-27 17:55:14 +0300

    intervals

--- src/litcode/interview/questions/easy/SummaryRanges.java
+++ src/litcode/interview/questions/easy/SummaryRanges.java
@@ -9,8 +9,13 @@ public class SummaryRanges {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             int start = nums[i];
-            while (nums[i++] + 1 != nums[i]) {
-                
+            while (i+1 < nums.length && (nums[i] + 1 == nums[i+1])) {
+                i++;
+            }
+            if(start != nums[i]){
+                list.add(String.format("%d -> %d",start,nums[i]));
+            }else {
+                list.add(String.valueOf(nums[i]));
             }
         }
         return list;
---
+++ src/litcode/interview/questions/medium/MergeIntervals.java
@@ -0,0 +1,34 @@
+package litcode.interview.questions.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] merge(int[][] intervals) {
+        List<int[]> mergeList = new ArrayList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int finish = intervals[i][1];
+            while (i + 1 < intervals.length && finish >= intervals[i + 1][0]) {
+                i++;
+                if (intervals[i][1] > finish) {
+                    finish = intervals[i][1];
+                }
+            }
+            mergeList.add(new int[]{start, finish});
+        }
+        int[][] merge = new int[mergeList.size()][2];
+        for (int i = 0; i < mergeList.size(); i++) {
+            merge[i][0] = mergeList.get(i)[0];
+            merge[i][1] = mergeList.get(i)[1];
+        }
+        return merge;
+    }
+}
