c5833d7817c0e2a291bb760bd82b2f788f8a6f0a
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-11-25 12:35:25 +0300

    stack

---
+++ src/litcode/heap/KthLargest.java
@@ -0,0 +1,32 @@
+package litcode.heap;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class KthLargest {
+
+    PriorityQueue<Integer> firstKElements;
+
+    public KthLargest(int k, int[] nums) {
+        firstKElements = new PriorityQueue<>(Comparator.comparingInt(o -> o));
+        if (nums.length > 0) {
+            Arrays.sort(nums);
+            for (int i = nums.length - 1; i >= nums.length - k; i--) {
+                firstKElements.add(nums[i]);
+            }
+        }
+    }
+
+    public int add(int val) {
+        if (firstKElements.isEmpty()) {
+            firstKElements.add(val);
+        }
+
+        if (val > firstKElements.peek()) {
+            firstKElements.poll();
+            firstKElements.add(val);
+        }
+        return firstKElements.peek();
+    }
+}
