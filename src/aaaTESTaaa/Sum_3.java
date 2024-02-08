e5729287f19252759d89bc72848f47c350925faf
Ramis <115800392+RamisAbdrakhmanov@users.noreply.github.com>
2024-02-08 22:20:01 +0300

    medium two_pointer

--- src/aaaTESTaaa/Sum_3.java
+++ src/aaaTESTaaa/Sum_3.java
@@ -7,23 +7,28 @@ import java.util.*;
 public class Sum_3 {
 
     public static void main(String[] args) throws InterruptedException {
+        Deque<Integer> deque = new LinkedList<>();
+        Collections.sort((LinkedList<Integer>) deque);
+        LinkedList<Integer> linkedList = (LinkedList<Integer>) deque;
+        linkedList.add(123);
+        Collections.sort(linkedList);
 
-    int[] a = new int[100];
-    for (int i = a.length - 1; i >= 0; i--) {
-        a[i] = (int) (Math.random() * 99);
+
+        int[] a = new int[100];
+        for (int i = a.length - 1; i >= 0; i--) {
+            a[i] = (int) (Math.random() * 99);
+        }
+        System.out.println(threeSum(a));
+
     }
-
-    System.out.println(threeSum(a));
-
-}
 
 
     public static List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> set = new HashSet<>();
-        for (int i = 0; i < nums.length-2; i++) {
-            for (int j = i+1; j < nums.length-1; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    if(nums[i] + nums[j] + nums[k] == 0){
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
                         List<Integer> list = new ArrayList<>(List.of(nums[i], nums[j], nums[k]));
                         Collections.sort(list);
                         set.add(list);
